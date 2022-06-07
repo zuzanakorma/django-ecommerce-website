c38bf35ed3bd31e7e389270e9980d901ce28ca48
Zuzana Kormancova <zunakorma@gmail.com>
2022-06-07 16:44:10 +0200

    cart functionality

--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Customer(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, null=True)
+    name = models.EmailField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
 
 
@@ -16,7 +16,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
 
@@ -39,6 +39,17 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+
+        return shipping
 
 
     @property
